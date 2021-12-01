eda49a4c4ecf2e17ff86e59715d673296a3d97b0
Angeloamenta <angeloamenta7@gmail.com>
2021-12-01 14:49:13 +0100

    selezione difficolta ok

--- js/script.js
+++ js/script.js
@@ -5,16 +5,41 @@
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
-// creo gli elementi che mi serviranno e li inserisco sull'html
+// // creo gli elementi che mi serviranno e li inserisco sull'html
 
-// seleziono il container
-const container = document.querySelector(".container");
-console.log(container);
-// creo header
-const header = document.createElement("header");
-// gli aggiungo la classe adatta
-header.classList.add("header");
-console.log(header);
-// lo metto dentro il mio container
-container.append(header);
+// // seleziono il container
+// const container = document.querySelector(".container");
+// console.log(container);
+// // creo header
+// const header = document.createElement("header");
+// // gli aggiungo la classe adatta
+// header.classList.add("header");
+// console.log(header);
+// // lo metto dentro il mio container
+// container.append(header);
+// // creo header dx e sx
+// const headerSx = document.createElement("div");
+// headerSx.classList.add("header-sx");
+// console.log(headerSx);
+// const headerDx = document.createElement("div");
+// headerDx.classList.add("header-dx");
+// console.log(headerDx);
+// header.append(headerSx, headerDx);
 
+
+const button = document.querySelector(".button-play");
+console.log(button);
+
+
+button.addEventListener("click", function() {
+    const easy = document.querySelector(".easy").value;
+    const medium = document.querySelector(".medium").value;
+    const hard = document.querySelector(".hard").value;
+
+    const difficulty = document.getElementById("difficulty").value;
+    
+    if (difficulty == "medium") {
+        alert("pippo");
+    }
+
+});
