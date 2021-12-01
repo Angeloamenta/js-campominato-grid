a060b3e7594fbf569b1c675c302a3245638e440a
Angeloamenta <angeloamenta7@gmail.com>
2021-12-01 14:27:01 +0100

    creazione header ed append

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+// creo gli elementi che mi serviranno e li inserisco sull'html
+
+// seleziono il container
+const container = document.querySelector(".container");
+console.log(container);
+// creo header
+const header = document.createElement("header");
+// gli aggiungo la classe adatta
+header.classList.add("header");
+console.log(header);
+// lo metto dentro il mio container
+container.append(header);
+
